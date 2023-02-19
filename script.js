84b737b4f91121a554c813d9f76d7c5d85a059d1
Colby Ricks <rickscolby@gmail.com>
2023-02-19 10:58:35 -0800

    Add all functionality, forgot to commit often lol

---
+++ script.js
@@ -0,0 +1,110 @@
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+
+const primaryInfo = document.getElementById('primary-info');
+const locationInfo = document.getElementById('location-info');
+const locationWeather = document.getElementById('location-weather');
+
+const locationName = document.getElementById('location-name');
+const locationRegion = document.getElementById('location-region');
+const currentTemp = document.getElementById('current-temp');
+const currentCondition = document.getElementById('current-condition');
+const currentConditionIcon = document.getElementById('current-condition-icon');
+const currentFeelsLike = document.getElementById('current-feels-like');
+
+let currentLocation = {};
+
+function getLocationInfo(city) {
+    return new Promise((resolve, reject) => {
+        fetch(`https://api.weatherapi.com/v1/current.json?key=4f349a250bb64e618a8211606231602&q=${city}&aqi=no`)
+        .then((response) => {return response.json()})
+        .then((data) => {setCurrentLocation(data)})
+        .catch((error) => {
+            locationName.textContent = "Location not found. Please try again";
+        })
+        .catch((error) => {console.log(error)});
+    });
+}
+
+function setCurrentLocation(data) {
+    currentLocation = data;
+    search.value = "";
+    populateDOM()
+}
+
+function populateDOM() {
+    // Adjust text content
+    locationName.textContent = currentLocation.location.name;
+    locationRegion.textContent = currentLocation.location.region;
+    currentLocation.current.degree = "f";
+    currentTemp.textContent = currentLocation.current.temp_f + "°";
+    currentTemp.classList = "fahrenheit";
+
+    currentCondition.textContent = currentLocation.current.condition.text;
+    currentConditionIcon.src = currentLocation.current.condition.icon;
+    currentFeelsLike.textContent = "Feels like: " + currentLocation.current.feelslike_f + "° F";
+    setConditionIcon()
+}
+
+function toggleDegrees() {
+    let degree = currentLocation.current.degree;
+    if (degree == "f") {
+        currentLocation.current.degree = "c";
+        currentTemp.classList = "celsius";
+        currentTemp.textContent = currentLocation.current.temp_c + "°";
+        currentFeelsLike.textContent = "Feels like: " + currentLocation.current.feelslike_c + "° C";
+    }
+    else {
+        currentLocation.current.degree = "f";
+        currentTemp.classList = "fahrenheit";
+        currentTemp.textContent = currentLocation.current.temp_f + "°";
+        currentFeelsLike.textContent = "Feels like: " + currentLocation.current.feelslike_f + "° F";
+    }
+}
+
+submit.addEventListener('click', async (e) => {
+    e.preventDefault();
+    if (search.value !== "") {
+        loadingInfo();
+        let location = await getLocationInfo(search.value);
+        // Update Dom
+        populateDOM();
+    }
+})
+
+currentTemp.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    toggleDegrees();
+}, false);
+
+
+function setConditionIcon() {
+    const condition = currentLocation.current.condition.text.toLowerCase();
+
+    if (condition.includes("sunny") || condition.includes('clear')) {
+        currentConditionIcon.classList = "fa-solid fa-sun";
+    } else if (condition.includes("cloudy") || condition.includes('overcast') || condition.includes('fog')) {
+        currentConditionIcon.classList = "fa-solid fa-cloud";
+    } else if (condition.includes("rain") || condition.includes('drizzle') || condition.includes("rain shower") || condition.includes('mist')) {
+        currentConditionIcon.classList = "fa-solid fa-cloud-rain";
+    } else if (condition.includes("thunder")) {
+        currentConditionIcon.classList = "fa-solid fa-cloud-bolt";
+    } else if (condition.includes('snow') || condition.includes('blizzard') || condition.includes('sleet') || condition.includes('snow shower')) {
+        currentConditionIcon.classList = "fa-solid fa-snowflake";
+    } else if (condition.includes('shower')) {
+        currentConditionIcon.classList = "fa-solid fa-cloud-showers-heavy";
+    } else {
+        currentConditionIcon.classList = "fa-solid fa-sun";
+    }
+}
+
+function loadingInfo() {
+    locationName.textContent = "Loading location information...";
+    locationRegion.textContent = "";
+    currentTemp.textContent = "";
+    currentTemp.classList = "";
+
+    currentCondition.textContent = ""
+    currentConditionIcon.classList = "";
+    currentFeelsLike.textContent = "";
+}
